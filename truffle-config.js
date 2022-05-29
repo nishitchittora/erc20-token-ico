3bb4ef627cb800fa5aea0200e682d04b9b00e299
Nishit Chittora <nishitchittora2015@gmail.com>
2022-05-29 15:34:06 +0530

    parsing added

--- truffle-config.js
+++ truffle-config.js
@@ -64,11 +64,12 @@ module.exports = {
     rinkeby: {
       provider: () =>
         new HDWalletProvider({
-          privateKeys: process.env.SECRET_KEY,
+          privateKeys: JSON.parse(process.env.SECRET_KEY),
           providerOrUrl: process.env.INFURA_URL,
           numberOfAddresses: 2,
+          derivationPath: "m/44'/60'/0'/0/",
         }),
-      network_id: 2, // Ropsten's id
+      network_id: 4, // Ropsten's id
       gas: 5500000, // Ropsten has a lower block limit than mainnet
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
