bbc361d1533bedfadc19947a322c13aa2b1cee7c
Nishit Chittora <nishitchittora2015@gmail.com>
2022-05-29 11:52:14 +0530

    NCToken contract initiated

---
+++ contracts/NCToken.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+
+contract NCToken{
+  constructor(){
+
+  }
+}
